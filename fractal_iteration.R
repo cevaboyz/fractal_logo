bf5b97c4525c3899372fe8ee824cf42836f8bd78
cevaboyz <andrea.castagna1994@gmail.com>
2021-04-28 21:43:01 +0200

    new structures

---
+++ fractal_iteration.R
@@ -0,0 +1,24 @@
+# function to run next iteration based on "ifun()"
+
+iterate <- function(object, ifun, ...)
+  
+  {
+    lineList <- vector("list", 0)
+    
+    for (i in 1:nrow(object))
+      
+      {
+        
+        old_line <- matrix(object[i,], nrow = 1)
+        
+        new_line <- ifun(old_line, ...)
+      
+        lineList[[length(lineList)+1]] <- new_line
+        
+      }
+    
+        new_object <- do.call(rbind, lineList)
+      
+        return(new_object)
+    
+    }
---
+++ fractal_tree.R
@@ -0,0 +1,17 @@
+# iterator function: recursive tree
+
+tree <- function(line0, angle = 30, reduce = .7, randomness = 0)
+  
+  {
+    angle1 <- angle+rnorm(1,0, randomness) #left branch
+    
+    angle2 <- -angle+rnorm(1,0,randomness) #right branch
+    
+    line1 <- newLine(line0, angle = angle1, reduce = reduce)
+    
+    line2 <- newlLine(line0, angle = angle2, reduce = reduce)
+    
+    mat <- matrix(c(line1,line2), byrow = T, ncol = 4)
+    
+    return(mat)
+  }
